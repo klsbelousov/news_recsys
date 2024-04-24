1b0e485457e09f89ac09ddefe941f211a507815a
Klim Belousov <klsbelousov@yandex.ru>
2024-04-24 14:59:40 +0300

    parser

--- crawlers/sports_crawler.py
+++ crawlers/sports_crawler.py
@@ -5,16 +5,23 @@ from bs4 import BeautifulSoup
 url = 'https://www.sports.ru/'
 
 response = requests.get(url=url)
-#print(response.text)
 
-soup = BeautifulSoup(response.text, 'lxml')
-data = soup.find('ul', class_='aside-news-list list-reset aside-news-block__list')
+main_page = BeautifulSoup(response.text, 'lxml')
+
+# Collecting top news
+top_title = main_page.find('h1', class_='super-top__title').text
+top_feature = main_page.find('p', class_='super-top__feature').text
+top_link = main_page.find('a', class_='super-top__link link link_color_white analyticsTrackClick').get('href')
+print(top_title + '\n' + top_feature + '\n' + top_link + '\n')
+
+# Collecting news
+data = main_page.find('ul', class_='aside-news-list list-reset aside-news-block__list')
 content = data.find_all('li' , class_='aside-news-list__item js-active')
-for i in content:
-    text = i.find('a', class_="analyticsTrackClick link link_size_small link_color_black").text
+for topic in content:
+    text = topic.find('a', class_="analyticsTrackClick link link_size_small link_color_black").text
     try:
-        fires = i.find('span', class_='link link_size_small link_color_blue comment-count__value').text
+        fires = topic.find('span', class_='link link_size_small link_color_blue comment-count__value').text
     except AttributeError:
         fires = 'None'
-    url_text = i.find('a', class_='analyticsTrackClick link link_size_small link_color_black').get('href')
-    print(text + '\n' + fires + '\n' + url_text + '\n')
+    topic_link = topic.find('a', class_='analyticsTrackClick link link_size_small link_color_black').get('href')
+    print(text + '\n' + fires + '\n' + topic_link + '\n')
