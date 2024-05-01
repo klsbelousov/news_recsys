567e5d28b3811dcf3a6a8c02aebd02dbe4f0bb28
Klim Belousov <klsbeousov@yandex.ru>
2024-05-01 10:00:18 +0300

    Connected crawler to DB

---
+++ crawlers/postgres.py
@@ -0,0 +1,56 @@
+import psycopg2
+from config import host, user, database, password
+#from crawlers import timestamp, category, title, link, text
+
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        database=database,
+        password=password,
+    )
+    
+    #creating a new table
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """CREATE TABLE sport_news(
+                id serial PRIMARY KEY,
+                timestamp varchar,
+                category varchar,
+                title text,
+                link varchar,
+                text text
+            )"""
+        )
+        connection.commit()
+        print('Table created')
+
+
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """
+    #         DROP TABLE sport_news
+    #         """
+    #     )
+    #     connection.commit()
+    #     print('Table was deleted')
+
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#             f"""
+#             INSERT INTO sport_news(timestamp, category, title, link, text)
+#             VALUES ({timestamp}, {category}, {title}, {link}, {text})
+#             """
+#         )
+#         print('[INFO] Data was successfully added')
+except Exception as error:
+    print(error)
+
+finally:
+    if connection:
+        connection.close()
+    
+    
+
+
+        # print("PostgreSQL connection closed")
--- crawlers/sports_crawler.py
+++ crawlers/sports_crawler.py
@@ -1,32 +1,57 @@
-import requests
 import time
 from random import randint
+
+import psycopg2
+import requests
 import lxml
 from bs4 import BeautifulSoup
+from config import host, user, password, database
+
+
 
 categories = ['football', 'hockey', 'basketball', 'boxing', 'tennis']
 url = 'https://www.sports.ru/'
 
-for category in categories:
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        database=database,
+        password=password,
+    )
 
-    full_url = url + category
-    time.sleep(randint(1, 6))
-    response = requests.get(url=full_url)
+    for category in categories:
 
-    main_page = BeautifulSoup(response.text, 'lxml')
+        full_url = url + category
+        time.sleep(randint(1, 6))
+        response = requests.get(url=full_url)
 
-    news_block = main_page.find_all('li', class_='aside-news-list__item js-active')
-    for block in news_block:
-        try:
-            timestamp = block.find('time').get('datetime')
-            title = block.find('a', class_='analyticsTrackClick link link_size_small link_color_black').text
-            link = block.find('a').get('href')
-            time.sleep(randint(1, 6))
-            page_response = requests.get(url=link)
-            page = BeautifulSoup(page_response.text, 'lxml')
-            topic = page.find('article', class_='news-item js-active')
-            text = topic.find('div', class_='news-item__content js-mediator-article').text
-            print(timestamp + '\n' + category + '\n' + '\n' + title + '\n' + link + '\n' + text + '\n\n\n')
-        except (AttributeError, ConnectionError, OSError) as error:
-            pass
+        main_page = BeautifulSoup(response.text, 'lxml')
+
+        news_block = main_page.find_all('li', class_='aside-news-list__item js-active')
+        for block in news_block:
+            try:
+                timestamp = block.find('time').get('datetime')
+                title = block.find('a', class_='analyticsTrackClick link link_size_small link_color_black').text
+                link = block.find('a').get('href')
+                time.sleep(randint(1, 6))
+                page_response = requests.get(url=link)
+                page = BeautifulSoup(page_response.text, 'lxml')
+                topic = page.find('article', class_='news-item js-active')
+                text = topic.find('div', class_='news-item__content js-mediator-article').text
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        f'INSERT INTO sport_news (timestamp, category, title, link, text) VALUES (%s, %s, %s, %s, %s);',
+                        (timestamp, category, title, link, text)
+                    )
+                    connection.commit()
+                    print('[INFO] Data was successfully added')
+            except (AttributeError, ConnectionError, OSError) as error:
+                pass
     
+except Exception as error:
+    print(error)
+
+finally:
+    if connection:
+        connection.close()
